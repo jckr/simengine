2633331b4e31c43d8387b2fd46b032b01efc3f8e
Jerome Cukier <jerome.cukier@datadoghq.com>
2024-03-18 13:51:04 -0700

    very basic version of engine working

--- src/components/react-engine.tsx
+++ src/components/react-engine.tsx
@@ -1,14 +1,15 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Engine } from '..';
 export function ReactEngine() {
-    const engineRef = React.useRef<Engine | null>(null);
-
+    const engineRef = React.useRef<Engine<number> | null>(null);
+    const [counter, setCounter] = useState<number>(0);
     React.useEffect(() => {
         if (engineRef.current === null) {
-            engineRef.current = new Engine();
+            engineRef.current = new Engine<number>({initialState: 0, onTick: counter => counter + 1, onUpdate: setCounter});
+            engineRef.current.play();
         }
     }, []);
 
-    return <div className="react-engine"></div>
+    return <div className="react-engine">{counter}</div>
 } 
